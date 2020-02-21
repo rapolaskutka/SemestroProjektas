45fae6c00a87b2722ec6a9be9dad80fb2ca0b5db
omegand <piktasdrakonas49@gmail.com>
2020-02-21 20:22:15 +0200

    Ikeliama is naujo

---
+++ BasePlatformer/Assets/Scripts/PlatformFallDown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFallDown : MonoBehaviour
+{
+    private PlatformEffector2D effect;
+    void Start()
+    {
+        effect = GetComponent<PlatformEffector2D>();
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow)) effect.rotationalOffset = 180f;
+        if (Input.GetKey(KeyCode.UpArrow)) effect.rotationalOffset = 0f;
+
+    }
+}
