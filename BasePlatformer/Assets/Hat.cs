14f0330684204217b89d39ca7f81da986f01c68c
omegand <piktasdrakonas49@gmail.com>
2020-02-28 11:53:34 +0200

    TG-37 Added hat Throwing
    Pressing z will make a hat fly off your body

---
+++ BasePlatformer/Assets/Hat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hat : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      // Destroy(gameObject);
+    }
+}
---
+++ BasePlatformer/Assets/Throwing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwing : MonoBehaviour
+{
+    public Transform StartPoint;
+
+    public GameObject HatPrefab;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Shoot();
+        }
+        
+    }
+    void Shoot() 
+    {
+        Instantiate(HatPrefab, StartPoint.position, StartPoint.rotation);
+    }
+}
