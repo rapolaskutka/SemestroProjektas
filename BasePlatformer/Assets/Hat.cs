571a922d0ac9973cc7edb47e30e09a89a84e7960
omegand <piktasdrakonas49@gmail.com>
2020-02-28 11:57:58 +0200

    TG-37 Hat explodes upon impact

--- BasePlatformer/Assets/Hat.cs
+++ BasePlatformer/Assets/Hat.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Hat : MonoBehaviour
 {
-    public float speed = 20f;
-    public Rigidbody2D rb;
+    public float speed = 15f;
+    private Rigidbody2D rb;
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * speed;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-      // Destroy(gameObject);
+       if(collision.name != "Character")
+       Destroy(gameObject);
     }
 }
