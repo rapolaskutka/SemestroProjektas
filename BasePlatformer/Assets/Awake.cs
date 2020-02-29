f55250ddbbd770cdb30ea0846ff983d84e8d9889
omegand <piktasdrakonas49@gmail.com>
2020-02-29 20:31:19 +0200

    TG-32 Added sprite rendering triggers

---
+++ BasePlatformer/Assets/Awake.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Awake : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Finish")gameObject.GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
