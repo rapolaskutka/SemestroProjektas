fa1225dbd92aa0c5c8ec5df96c46335f1217fdc0
omegand <piktasdrakonas49@gmail.com>
2020-02-29 23:00:08 +0200

    Game closes after finishing it

---
+++ BasePlatformer/Assets/CountDownTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startTime = 15f;
+
+    public Text CountDownText;
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= 1 * Time.deltaTime;
+            CountDownText.text = currentTime.ToString("00");
+            CountDownText.color = Color.red;
+        }
+        else Application.Quit();
+    }
+}
