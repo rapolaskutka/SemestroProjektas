8a7c73fe2772c497b6c38fcab90ee680c7c965ee
omegand <piktasdrakonas49@gmail.com>
2020-03-01 22:25:47 +0200

    Added a Main menu

---
+++ BasePlatformer/Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+     public void PlayGame() 
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene("Best");
+    }
+}
