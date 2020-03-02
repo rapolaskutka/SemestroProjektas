be3a5ef64126377ea7903ea635072bd408259386
omegand <piktasdrakonas49@gmail.com>
2020-03-02 22:33:42 +0200

    Added Pause Menu

---
+++ BasePlatformer/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool Paused = false;
+    public GameObject UI;
+    private void Start()
+    {
+      UI.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused) Resume();
+            else Pause();
+        }
+        Debug.Log(Paused);
+    }
+    public void Resume()
+    {
+        UI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+    void Pause()
+    {
+        UI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene("Best");
+    }
+}
