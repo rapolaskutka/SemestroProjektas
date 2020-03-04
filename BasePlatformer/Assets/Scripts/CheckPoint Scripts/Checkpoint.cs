f0a1d40e4a1d7b041cd54ef1e1d9f8222a8f2565
omegand <piktasdrakonas49@gmail.com>
2020-03-04 19:16:46 +0200

    TG-45 #in-progress Added Checkpoint triggers

---
+++ BasePlatformer/Assets/Scripts/CheckPoint Scripts/Checkpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private GM gamemaster;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) { gamemaster.CheckpointPos = transform.position; }
+    }
+    void Start() 
+    {
+        gamemaster = GameObject.FindGameObjectWithTag("GM").GetComponent<GM>();
+    }
+}
