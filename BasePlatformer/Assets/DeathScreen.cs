2b16c1cc03cb10d4f9c15ceaf3810adbb6d82c4a
omegand <piktasdrakonas49@gmail.com>
2020-03-08 20:24:20 +0200

    TG-50 #done Added death screen

---
+++ BasePlatformer/Assets/DeathScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+    private GameObject UI;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        { Destroy(GameObject.FindGameObjectWithTag("Player")); UI.SetActive(true); }
+
+    }
+    private void Awake()
+    {
+        UI = GameObject.FindGameObjectWithTag("DScreen");
+    }
+    private void Start()
+    {
+
+        UI.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
