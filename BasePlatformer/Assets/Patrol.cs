4a1f039470dafd643e0e9f4ba2b453015cda2811
omegand <piktasdrakonas49@gmail.com>
2020-03-11 01:22:20 +0200

    TG-42 Enemy will actively dodge you

--- BasePlatformer/Assets/Patrol.cs
+++ BasePlatformer/Assets/Patrol.cs
@@ -7,19 +7,21 @@ public class Patrol : MonoBehaviour
     public float speed;
     private bool movingRight = true;
     public Transform Ground;
-    private int layer_mask;
+    [SerializeField]
+    private LayerMask Hitswhat;
     public GameObject deathparticles;
-    private void Start()
-    {
-        layer_mask = LayerMask.GetMask("Walls");
-    }
     private void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D ground = Physics2D.Raycast(Ground.position, Vector2.down, 2f, Hitswhat);
+        RaycastHit2D personright = Physics2D.Raycast(Ground.position, Vector2.right, 0.3f, Hitswhat);
+        RaycastHit2D personleft = Physics2D.Raycast(Ground.position, Vector2.left, 0.3f, Hitswhat);
+        if (ground.collider == false) Collision();
+        if (personright.collider  == true || personleft.collider == true) Collision(); 
+    }
 
-        RaycastHit2D ground = Physics2D.Raycast(Ground.position, Vector2.down, 2f, layer_mask);
-
-        if (ground.collider == false)
+    private void Collision()
+    {
             if (movingRight == true)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
@@ -31,6 +33,7 @@ public class Patrol : MonoBehaviour
                 movingRight = true;
             }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Hat"))
