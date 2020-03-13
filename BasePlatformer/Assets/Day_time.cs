573991a57c72a38a7b5bc22307fb42775b10c22e
rapolaskutka <kutkarapolas@gmail.com>
2020-03-13 21:25:09 +0200

    TG-56 #In_progress Created general form
    Added dunes,  sky and sun.

---
+++ BasePlatformer/Assets/Day_time.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day_time : MonoBehaviour  
+{
+
+    public float speed;
+    public float Radius = 0.1f;
+
+    private Vector2 centre;
+    private float angle;
+
+    void Start()
+    {
+        centre.x = transform.position.x - 140;
+        centre.y = transform.position.y - 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += -speed * Time.deltaTime;
+
+        var offset = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * Radius;
+        transform.position = centre - offset;
+        //transform.Translate(new Vector3(speed * -Mathf.Cos(Time.deltaTime/2), speed * Mathf.Cos(Time.deltaTime/2), 0));
+    }
+}
