c1263e38201ed29de3ad7ad77ce3bce83b2a4995
omegand <piktasdrakonas49@gmail.com>
2020-04-17 20:04:17 +0300

    TG-70 Fixed wall stuck bug

--- BasePlatformer/Assets/CloneTeleport.cs
+++ BasePlatformer/Assets/CloneTeleport.cs
@@ -5,6 +5,7 @@ public class CloneTeleport : MonoBehaviour
     [SerializeField] private float speed = 15f;
     [SerializeField] private GameObject impact;
     private Rigidbody2D rb;
+    private Rigidbody2D PlayerRb;
     private Throwing throwclass;
     public Vector2 Pos;
     void Start()
@@ -13,14 +14,17 @@ public class CloneTeleport : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * speed;
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Ground"))
         {
+            PlayerRb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
             Instantiate(impact, transform.position, Quaternion.identity);
             throwclass.RemoveCooldownGhost();
-            throwclass.ChangePos(transform.position);
+            PlayerRb.velocity = Vector2.zero;
+            if(throwclass.Threwright) throwclass.ChangePos(transform.position - (Vector3.right * 0.3f));
+            else throwclass.ChangePos(transform.position + (Vector3.right * 0.3f));
+
             Destroy(this.gameObject);
         }
 
